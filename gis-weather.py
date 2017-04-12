3927175b0c300d6d7e846d923badf6e2fec825a6
RingOV <ringov@mail.ru>
2017-04-12 22:10:51 +0300

    fix

--- gis-weather.py
+++ gis-weather.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #
 #  gis_weather.py
-v = '0.8.2.44'
+v = '0.8.2.45'
 #  Copyright (C) 2013-2017 Alexander Koltsov <ringov@mail.ru>
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -904,7 +904,6 @@ class MyDrawArea(Gtk.DrawingArea):
                 os.remove(os.path.join(path_to_save, 'cur_temp'))
             process = subprocess.Popen(['/bin/bash'], shell=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True)
             process.stdin.write('echo "'+t_now[0].split(';')[t_index]+t_now_post+'" > '+os.path.join(path_to_save, 'cur_temp')+'\n')
-            process.terminate()
             print(os.path.join(path_to_save, 'cur_temp')+' saved (%s)'%t_now[0].split(';')[t_index]+t_now_post)
 
         if save_weather:
