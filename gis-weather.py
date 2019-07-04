391c8399251b8427447da5a937aae1a59b2e87d1
RingOV <ringov@mail.ru>
2019-07-04 20:56:50 +0300

    Update l10n

--- gis-weather.py
+++ gis-weather.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #
 #  gis_weather.py
-v = '0.8.3.14'
+v = '0.8.3.15'
 #  Copyright (C) 2013-2019 Alexander Koltsov <ringov@mail.ru>
 #
 #    This program is free software: you can redistribute it and/or modify
