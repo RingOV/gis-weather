fafd8845d28411276cfe3f53e18ea978b272c3c2
RingOV <ringov@mail.ru>
2021-01-20 18:33:22 +0300

    2021

--- gis-weather.py
+++ gis-weather.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 #
 #  gis_weather.py
-v = '0.8.4.14'
-#  Copyright (C) 2013-2020 Alexander Koltsov <ringov@mail.ru>
+v = '0.8.4.15'
+#  Copyright (C) 2013-2021 Alexander Koltsov <ringov@mail.ru>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
